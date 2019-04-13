938fdd0692a8a13ccedf90b16582aec2424cefa8
Willian Antunes <willian.lima.antunes@gmail.com>
2019-04-13 19:09:18 -0300

    Log setup following Django pattern

--- flask_playground/config/settings.py
+++ flask_playground/config/settings.py
@@ -1,4 +1,7 @@
 import os
+from logging import Formatter
+
+from pythonjsonlogger.jsonlogger import JsonFormatter
 
 HOST = os.getenv("WA_HOST", "0.0.0.0")
 PORT = int(os.getenv("WA_PORT", 8000))
@@ -7,3 +10,27 @@ FLASK_DEBUG = os.getenv("WA_FLASK_DEBUG", True)
 
 GITHUB_GRAPHQL_ENDPOINT = os.getenv("WA_GITHUB_GRAPHQL_ENDPOINT")
 GITHUB_API_TOKEN = os.getenv("WA_GITHUB_API_TOKEN")
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "std-json-formatter": {
+            "()": JsonFormatter,
+            "format": "%(levelname)-8s [%(asctime)s] [%(request_id)s] %(name)s: %(message)s",
+        },
+        "std-formatter": {"()": Formatter, "format": "%(asctime)s - level=%(levelname)s - %(name)s - %(message)s"},
+    },
+    "handlers": {
+        "console": {"class": "logging.StreamHandler", "formatter": os.getenv("WA_LOG_FORMATTER", "std-formatter")}
+    },
+    "loggers": {
+        "": {"level": os.getenv("WA_ROOT_LOG_LEVEL", "INFO"), "handlers": ["console"]},
+        "flask_playground": {
+            "level": os.getenv("WA_PROJECT_LOG_LEVEL", "INFO"),
+            "handlers": ["console"],
+            "propagate": False,
+        },
+        "flask": {"level": os.getenv("WA_FLASK_LOG_LEVEL", "DEBUG"), "propagate": False, "handlers": ["console"]},
+    },
+}
--- init.py
+++ init.py
@@ -1,7 +1,10 @@
+import logging.config
+
 from flask import Flask
 
 from flask_playground.config.settings import FLASK_DEBUG
 from flask_playground.config.settings import HOST
+from flask_playground.config.settings import LOGGING
 from flask_playground.config.settings import PORT
 from flask_playground.routes.v1 import api_v1_routes
 
@@ -9,4 +12,5 @@ app = Flask(__name__)
 app.register_blueprint(api_v1_routes, url_prefix="/api")
 
 if __name__ == "__main__":
+    logging.config.dictConfig(LOGGING)
     app.run(debug=FLASK_DEBUG, host=HOST, port=PORT)
