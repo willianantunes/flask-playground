175b7d39c1bee6bee15906cfe24e837ad927f1ac
Willian Antunes <willian.lima.antunes@gmail.com>
2019-04-13 16:26:27 -0300

    Refactory to reuse same mock logic

--- tests/unit/business/test_user_dealer.py
+++ tests/unit/business/test_user_dealer.py
@@ -4,9 +4,7 @@ from flask_playground.business import user_dealer
 
 
 def test_should_be_eligible_when_number_of_followers_greather_than_10(mocker: MockFixture):
-    mocked_github_graphql_api = mocker.patch("flask_playground.business.user_dealer.github_graphql_api")
-    mocked_total_number_of_followers = mocked_github_graphql_api.total_number_of_followers
-    mocked_total_number_of_followers.return_value = 11
+    mocked_github_graphql_api, mocked_total_number_of_followers = _prepare_github_graphql_api_mock(mocker, 11)
     fake_username_one = "fake-username-one"
 
     result = user_dealer.is_eligible(fake_username_one)
@@ -14,9 +12,9 @@ def test_should_be_eligible_when_number_of_followers_greather_than_10(mocker: Mo
     mocked_total_number_of_followers.assert_called_once_with(fake_username_one)
     assert result is True
 
-    mocked_github_graphql_api.reset_mock()
-    mocked_total_number_of_followers = mocked_github_graphql_api.total_number_of_followers
-    mocked_total_number_of_followers.return_value = 500
+    _, mocked_total_number_of_followers = _prepare_github_graphql_api_mock(
+        mocker, 500, created_mock=mocked_github_graphql_api
+    )
     fake_username_two = "fake-username-two"
 
     result = user_dealer.is_eligible(fake_username_two)
@@ -26,9 +24,7 @@ def test_should_be_eligible_when_number_of_followers_greather_than_10(mocker: Mo
 
 
 def test_should_not_be_eligible_when_number_of_followers_less_or_equal_than_10(mocker: MockFixture):
-    mocked_github_graphql_api = mocker.patch("flask_playground.business.user_dealer.github_graphql_api")
-    mocked_total_number_of_followers = mocked_github_graphql_api.total_number_of_followers
-    mocked_total_number_of_followers.return_value = 10
+    mocked_github_graphql_api, mocked_total_number_of_followers = _prepare_github_graphql_api_mock(mocker, 10)
     fake_username_one = "fake-username-one"
 
     result = user_dealer.is_eligible(fake_username_one)
@@ -36,12 +32,24 @@ def test_should_not_be_eligible_when_number_of_followers_less_or_equal_than_10(m
     mocked_total_number_of_followers.assert_called_once_with(fake_username_one)
     assert result is False
 
-    mocked_github_graphql_api.reset_mock()
-    mocked_total_number_of_followers = mocked_github_graphql_api.total_number_of_followers
-    mocked_total_number_of_followers.return_value = 1
+    _, mocked_total_number_of_followers = _prepare_github_graphql_api_mock(
+        mocker, 1, created_mock=mocked_github_graphql_api
+    )
     fake_username_two = "fake-username-two"
 
     result = user_dealer.is_eligible(fake_username_two)
 
     mocked_total_number_of_followers.assert_called_once_with(fake_username_two)
     assert result is False
+
+
+def _prepare_github_graphql_api_mock(mocker, total_number_of_followers_value, created_mock=None):
+    mocked_github_graphql_api = (
+        mocker.patch("flask_playground.business.user_dealer.github_graphql_api")
+        if created_mock is None
+        else created_mock
+    )
+    mocked_github_graphql_api.reset_mock()
+    mocked_total_number_of_followers = mocked_github_graphql_api.total_number_of_followers
+    mocked_total_number_of_followers.return_value = total_number_of_followers_value
+    return mocked_github_graphql_api, mocked_total_number_of_followers
